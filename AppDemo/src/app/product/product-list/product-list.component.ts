75ffaa1989a1ffd514fe53478bce9caf88769ec7
Conan <votunglamdhsp@gmail.com>
2019-10-02 18:20:09 +0700

    submit view and delete products

--- AppDemo/src/app/product/product-list/product-list.component.ts
+++ AppDemo/src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService, product } from 'src/app/service/product.service';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: product[];
+  currentProduct: product = null;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+  ) { }
 
   ngOnInit() {
+    this.getListProducts();
   }
 
+  getListProducts(){
+    this.productService.getProducts().subscribe(data =>{
+      this.products = data;
+    })
+  }
+ 
+  viewProduct(product:product){
+    this.currentProduct = product;
+  }
+
+  addIssue(){
+    alert('Chức năng đang hoàn thiện!')
+  }
+
+  deleteProduct(id:number){
+    console.log(id);
+    this.productService.deleteProduct(id);
+    this.getListProducts();
+  }
+  
 }
--- AppDemo/src/app/service/product.service.ts
+++ AppDemo/src/app/service/product.service.ts
@@ -1,9 +1,104 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+
+export interface product {
+  id: number,
+  name: string,
+  description: string,
+  price: number,
+  amount: number
+}
+
+const products: product[] = [
+  {
+    id: 1,
+    name: 'Iphone Z',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 2,
+    name: 'Iphone A',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 3,
+    name: 'Iphone B',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 4,
+    name: 'Iphone C',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 5,
+    name: 'Iphone D',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 6,
+    name: 'Iphone E',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 7,
+    name: 'Iphone F',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 8,
+    name: 'Iphone G',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 9,
+    name: 'Iphone H',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  },
+  {
+    id: 10,
+    name: 'Iphone I',
+    description: 'This is description for phone.',
+    price: 8000000,
+    amount: 3
+  }
+]
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  currentRemove: number;
+
 
   constructor() { }
+
+  getProducts() {
+    return of(products);
+  }
+
+  deleteProduct(id:number){
+    const index = products.findIndex(data => data.id === id);
+    console.log(index);
+    products.splice(index, 1);
+  }
+
 }
